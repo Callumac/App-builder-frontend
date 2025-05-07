060bd67ebe12de84cbeb83f8861014e76b489c44
Callumac <convertgenius.zone@gmail.com>
2025-05-07 02:42:27 +0100

    Create Loader.tsx

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export function Loader() {
+  return (
+    <div className="flex items-center justify-center p-8">
+      <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
+    </div>
+  );
+}
